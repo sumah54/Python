f25f65efd566a590934eeb37286ba040be2e1ca6
Ahsan <sumah6437@gmail.com>
2021-11-06 17:03:17 +0500

    Delete cat asker.py

--- cat asker.py
+++
@@ -1,9 +0,0 @@
-names = []
-while True:
-	name = input("enter the name of cat "+ str(len(names)+ 1) + '(or enter nothing to stop.)')
-	if name == '':
-		break
-	names = names + [name]
-print("the name of cats are :")
-for name in names:
-	print("  "+ name)
